f59c634427eadbe1bfa16afcba5f09ac15031581
Jon Gjerset <jon@gjerset.com>
2012-10-03 16:59:50 -0700

    AdminLogin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 SwagShack::Application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+
+  resources :users
+
   get "store/index"
 
   resources :products
